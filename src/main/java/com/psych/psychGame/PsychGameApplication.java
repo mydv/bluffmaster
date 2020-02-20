dd4a32811ab8b12730f4af6b0a148ac210196ad8
Mohit Yadav <du.mohit93@gmail.com>
2020-02-20 18:55:28 +0530

    init

---
+++ src/main/java/com/psych/psychGame/PsychGameApplication.java
@@ -0,0 +1,13 @@
+package com.psych.psychGame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsychGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsychGameApplication.class, args);
+	}
+
+}
